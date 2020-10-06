2716c8012bd0867e21919e0eea55628084f4c9d6
mike hill <michael-hill@lambdastudents.com>
2020-10-06 15:27:03 -0400

    intial setup

---
+++ src/featureReducer/featureReducer.js
@@ -0,0 +1,13 @@
+
+
+
+const initialState = {
+
+}
+
+export const featureReducer = (state = initialState, action) => {
+    switch(action.type){
+        default:
+            return state
+    }
+}
